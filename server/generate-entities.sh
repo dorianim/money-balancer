b914205ee97b74a7d2a7f70bdc572698a5eab433
Dorian Zedler <dorian@itsblue.de>
2022-09-25 18:50:54 +0200

    Feat: run migrations before generating entities

--- server/generate-entities.sh
+++ server/generate-entities.sh
@@ -1,3 +1,7 @@
-sea-orm-cli generate entity \
-    -u "sqlite:./money-balancer.sqlite?mode=rwc" \
-    -o src/model
+export DATABASE_URL="sqlite:./money-balancer.sqlite?mode=rwc"
+
+while [[ "$(sea-orm-cli migrate status)" == *"Pending"* ]]; do
+    sea-orm-cli migrate up
+done
+
+sea-orm-cli generate entity -o src/model
